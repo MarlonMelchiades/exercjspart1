ff3d8cc6ac246efe73a3858c56a97f7374b81b7f
marlonmelchiades <79936558+MarlonMelchiades@users.noreply.github.com>
2021-10-11 16:50:42 -0300

    Add files via upload

---
+++ exercPart1.js
@@ -0,0 +1,466 @@
+// ----- exercício 1 ----- //
+
+function calcularOperacoes (operador1, operador2) {
+    console.log(operador1+operador2, operador1-operador2, operador1*operador2, operador1/operador2);
+}
+
+calcularOperacoes(1, 2)
+
+// ----- exercício 2 ----- //
+
+function classificaTriangulo (lado1, lado2, lado3) {
+    if(lado1 == lado2 && lado2 == lado3){
+        return 'Equilátero'
+    } else if (lado1 == lado2 || lado2 == lado3 || lado1 == lado3) {
+        return 'Isósceles'        
+    }else { 
+        return 'Escaleno'
+    }
+}
+
+console.log(classificaTriangulo(2, 2, 2));
+console.log(classificaTriangulo(2, 3, 3));
+console.log(classificaTriangulo(2, 3, 4));
+
+// ----- exercício 3 ----- //
+
+function expoente (base, expoente) {
+    //Método antigo:
+    let resultado = Math.pow(base, expoente)
+    //Método novo:
+    resultado = base ** expoente
+
+    return resultado
+}
+
+console.log(expoente(2, 3))
+
+// ----- exercício 4 ----- //
+
+function divisao (dividendo, divisor) {
+    console.log("Resultado: " + Math.floor(dividendo/divisor));
+    console.log(`Resto: ${dividendo % divisor}`)
+}
+
+divisao(11, 4)
+
+// ----- exercício 5 ----- //
+
+function formatarValorDecimal(valorDecimal) {
+    valorEmReais = `R$ ${valorDecimal.toFixed(2).toString().replace(".", ",")}`
+    console.log(valorEmReais)
+}
+
+formatarValorDecimal(0.1 + 0.2)
+
+// ----- exercício 6 ----- //
+
+function jurosSimples (capitalInicial, taxa, tempo) {
+    return capitalInicial + (capitalInicial * taxa * tempo)
+}
+
+function jurosCompostos (capitalInicial, taxa, tempo) {
+    return capitalInicial * (1 + taxa) ** tempo
+}
+
+console.log(jurosSimples(100, 10/100, 2));
+console.log(jurosCompostos(100, 10/100, 2));
+
+// ----- exercício 7 ----- //
+
+function bhaskara (ax2, bx, c) {
+    let resultados = []
+    let delta = (bx ** 2) - (4 * ax2 * c)
+    if(delta < 0){
+        return "Delta é negativo"
+    } 
+    let x1 = (-bx + Math.sqrt(delta))/(2*ax2)
+    let x2 = (-bx - Math.sqrt(delta))/(2*ax2)
+    resultados.push(x1)
+    resultados.push(x2)
+    return resultados   
+}
+
+console.log(bhaskara(1, 3, 2))
+console.log(bhaskara(3, 1, 2))
+
+// ----- exercício 8 ----- //
+
+let stringPontuacoes = "30, 40, 20, 4, 51, 25, 42, 38, 56, 0"
+ 
+ 
+function avaliaPontuacoes (stringPontuacoes) {
+    let pontuacoes = stringPontuacoes.split(", ")
+    let qtdQuebraDeRecords = 0
+    let piorJogo = 1
+    let maiorPontuacao = pontuacoes[0]
+    let menorPontuacao = pontuacoes[0]
+
+    for (let i = 1; i < pontuacoes.length; i++) {
+        if(pontuacoes[i] > maiorPontuacao) {
+            maiorPontuacao = pontuacoes[i]
+            qtdQuebraDeRecords++
+        }else if (pontuacoes[i] < menorPontuacao) {
+            menorPontuacao = pontuacoes[i]
+            piorJogo = i+1;
+        }
+    }
+    return [qtdQuebraDeRecords, piorJogo]
+}
+ 
+console.log(avaliaPontuacoes(stringPontuacoes))
+
+// ----- exercício 9 ----- //
+
+function classificaAluno(nota) {
+    let notaCorrigida = arredondar(nota)
+    if (notaCorrigida >= 40) {
+        console.log(`Aprovado com nota ${notaCorrigida}`);
+    } else {
+        console.log(`Reprovado com nota ${notaCorrigida}`);
+    }
+}
+
+function arredondar (nota) {
+    if (nota % 5 > 2) {
+        return nota + (5 - (nota % 5))
+    } else {
+        return nota
+    }
+}
+
+classificaAluno(100)
+classificaAluno(30)
+classificaAluno(38)
+classificaAluno(88)
+classificaAluno(61)
+
+// ----- exercício 10 ----- //
+
+function verificaNumero (numero) {
+    if(numero % 3 == 0){
+        return true
+    }else{
+        return false
+    }
+}
+
+console.log(verificaNumero(3))
+console.log(verificaNumero(2))
+console.log(verificaNumero(150))
+
+// ----- exercício 11 ----- //
+
+function calcularAnoBissexto (ano) {
+    if (ano <= 0){
+        return false
+    } else if (ano % 400 == 0) {
+        return true
+    } else if (ano % 100 == 0) {
+        return false
+    } else if (ano % 4 == 0) {
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(calcularAnoBissexto(0))
+console.log(calcularAnoBissexto(4))
+console.log(calcularAnoBissexto(100))
+console.log(calcularAnoBissexto(400))
+console.log(calcularAnoBissexto(800))
+console.log(calcularAnoBissexto(2020))
+console.log(calcularAnoBissexto(2021))
+
+// ----- exercício 12 ----- //
+
+function fatorial (numero) {
+    if(numero == 0){
+        return 1
+    } else {
+        return numero * fatorial(numero - 1)
+    }
+}
+
+console.log(fatorial(10))
+
+// ----- exercício 13 ----- //
+
+function classificaDia(numero) {
+    switch (numero) {
+        case 1:
+            return "Fim de semana"
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6: 
+            return "Dia útil"
+        case 7:
+            return "Fim de semana"
+        default: 
+            return "Dia inválido"
+    }
+}
+
+console.log(classificaDia(1));
+console.log(classificaDia(2));
+console.log(classificaDia(3));
+console.log(classificaDia(4));
+console.log(classificaDia(5));
+console.log(classificaDia(6));
+console.log(classificaDia(7));
+console.log(classificaDia('a'));
+
+// ----- exercício 14 ----- //
+
+function venderFruta (fruta) {
+    switch (fruta) {
+        case 'maçã':
+            console.log('Não vendemos esta fruta aqui.')
+            break
+        case 'kiwi':
+            console.log('Estamos com escassez de kiwi.')
+            break
+        case 'melancia':
+            console.log('Aqui está, custa R$ 3,00 o quilo.')
+            break
+        default:
+            console.log('Erro, fruta inválida.')
+    }
+}
+
+venderFruta('maçã')
+venderFruta('kiwi')
+venderFruta('melancia')
+venderFruta('alcatra')
+
+// ----- exercício 15 ----- //
+
+function escolherCarro (modelo) {
+    switch (modelo) {
+        case 'hatch':
+            return 'Compra efetuada com sucesso.'
+        case 'sedan':
+        case 'motocicleta':
+        case 'caminhonete':
+            return 'Tem certeza que não prefere este modelo?'
+        default:
+            return 'Não trabalhamos com este tipo de automóvel aqui.'
+    }
+}
+
+console.log(escolherCarro('hatch'));
+console.log(escolherCarro('motocicleta'));
+console.log(escolherCarro('sedan'));
+console.log(escolherCarro('caminhonete'));
+console.log(escolherCarro('jetski'));
+
+// ----- exercício 16 ----- //
+
+function calculadora (valor1, operacao, valor2) {
+    switch (operacao) {
+        case '+':
+            return valor1 + valor2
+        case '-':
+            return valor1 - valor2
+        case '*':
+            return valor1 * valor2
+        case '/':
+            return valor1 / valor2
+        default:
+            return 'Operação inválida'
+    }
+}
+
+console.log(calculadora(2, '+', 3));
+console.log(calculadora(2, '-', 3));
+console.log(calculadora(2, '*', 3));
+console.log(calculadora(2, '/', 3));
+console.log(calculadora(2, 'a', 3));
+
+// ----- exercício 17 ----- //
+
+function calcularReajuste(plano, salarioAtual) {
+    switch (plano) {
+        case 'A':
+            return salarioAtual * 1.1
+        case 'B': 
+            return salarioAtual * 1.15
+        case 'C':
+            return salarioAtual * 1.2
+        default:
+            return 'Plano inválido.'
+    }
+}
+
+console.log(calcularReajuste('A', 1000));
+console.log(calcularReajuste('B', 1000));
+console.log(calcularReajuste('C', 1000));
+console.log(calcularReajuste('D', 1000));
+
+// ----- exercício 18 ----- //
+
+function numeroPorExtenso(numero) {
+    switch (numero) {
+        case 0:
+            console.log('Zero')
+            break
+        case 1:
+            console.log('Um')
+            break
+        case 2:
+            console.log('Dois')
+            break
+        case 3:
+            console.log('Três')
+            break
+        case 4:
+            console.log('Quatro')
+            break
+        case 5:
+            console.log('Cinco')
+            break
+        case 6:
+            console.log('Seis')
+            break
+        case 7:
+            console.log('Sete')
+            break
+        case 8:
+            console.log('Oito')
+            break
+        case 9:
+            console.log('Nove')
+            break
+        case 10:
+            console.log('Dez')
+            break
+        default:
+            console.log('Numero fora do intervalo.')
+    }
+}
+
+numeroPorExtenso(0)
+numeroPorExtenso(1)
+numeroPorExtenso(2)
+numeroPorExtenso(3)
+numeroPorExtenso(4)
+numeroPorExtenso(5)
+numeroPorExtenso(6)
+numeroPorExtenso(7)
+numeroPorExtenso(8)
+numeroPorExtenso(9)
+numeroPorExtenso(10)
+numeroPorExtenso(11)
+
+// ----- exercício 19 ----- //
+
+function calcularVenda(codigoProduto, qtd) {
+    switch (codigoProduto) {
+        case 100:
+            return qtd * 3
+        case 200:
+            return qtd * 4
+        case 300:
+            return qtd * 5.5
+        case 400:
+            return qtd * 7.5
+        case 500:
+            return qtd * 3.5
+        case 600:
+            return qtd * 2.8
+        default:
+            return 'Produto não existente.'
+    }
+}
+
+console.log(calcularVenda(100, 2))
+console.log(calcularVenda(200, 2))
+console.log(calcularVenda(300, 2))
+console.log(calcularVenda(400, 2))
+console.log(calcularVenda(500, 2))
+console.log(calcularVenda(600, 2))
+console.log(calcularVenda(700, 2))
+
+// ----- exercício 20 ----- //
+
+function sacarDinheiro(valorSaque) {
+    let contador100 = 0
+    let contador50 = 0
+    let contador10 = 0
+    let contador5 = 0
+    let contador1 = 0
+    let valorNota = calcularValorNota(valorSaque)
+    while (valorSaque >= valorNota) {
+        switch (valorNota) {
+            case 100:
+                valorSaque -= 100
+                contador100++
+                break
+            case 50:
+                valorSaque -= 50
+                contador50++
+                break
+            case 10:
+                valorSaque -= 10
+                contador10++
+                break
+            case 5:
+                valorSaque -= 5
+                contador5++
+                break
+            case 1:
+                contador1++
+                valorSaque -= 1
+                break
+        }
+
+        valorNota = calcularValorNota(valorSaque)
+
+    }
+    return elaborarResultado(contador100, contador50, contador10, contador5, contador1)
+}
+
+function calcularValorNota(valorSaque) {
+    if (valorSaque >= 100) {
+        return 100
+    } else if (valorSaque >= 50) {
+        return 50
+    } else if (valorSaque >= 10) {
+        return 10
+    } else if (valorSaque >= 5) {
+        return 5
+    } else if (valorSaque >= 1) {
+        return 1
+    }
+}
+
+function elaborarResultado(contador100, contador50, contador10, contador5, contador1) {
+    let resultado = ''
+
+    if (contador100 > 0) {
+        resultado += `${contador100} nota(s) de R$ 100. `
+    }
+
+    if (contador50 > 0) {
+        resultado += `${contador50} nota(s) de R$ 50. `
+    }
+
+    if (contador10 > 0) {
+        resultado += `${contador10} nota(s) de R$ 10. `
+    }
+
+    if (contador5 > 0) {
+        resultado += `${contador5} nota(s) de R$ 5. `
+    }
+
+    if (contador1 > 0) {
+        resultado += `${contador1} nota(s) de R$ 1. `
+    }
+
+    return resultado
+}
+
+console.log(sacarDinheiro(153));
